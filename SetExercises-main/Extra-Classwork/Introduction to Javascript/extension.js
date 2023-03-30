290bb4f5532d63313b9adf82e04355747079433c
Abdul Hadi <abdulhadi2003m@gmail.com>
2023-03-30 16:57:01 +0400

    .

---
+++ SetExercises-main/Extra-Classwork/Introduction to Javascript/extension.js
@@ -0,0 +1,14 @@
+alert("Welcome");
+alert("Hello");
+
+var name_1 = "Hello";
+
+document.write(name_1);
+document.getElementById("A").innerHTML = 5 + 6;
+
+var name = "expression";
+var enrollment = "99";
+var mediangrade = "2.8";
+var credits = 5 + 4(2 * 3);
+
+document.write(name, " ", enrollment, " ", mediangrade, " ", credits);
