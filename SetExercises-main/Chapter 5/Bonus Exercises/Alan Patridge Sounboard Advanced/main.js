cbc3d4be58941248890e9aa8006a84c4491aee43
AbdulHadi <104209042+AbdulHadi20@users.noreply.github.com>
2023-04-30 23:39:34 +0400

    commit

--- SetExercises-main/Chapter 5/Bonus Exercises/Alan Patridge Sounboard Advanced/main.js
+++ SetExercises-main/Chapter 5/Bonus Exercises/Alan Patridge Sounboard Advanced/main.js
@@ -14,3 +14,16 @@ window.addEventListener("load", () => {
         });
     });
 });
+
+//text-to-speech 
+
+function textToAudio() {
+    let msg = document.getElementById("text-to-speech").value;
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    speech.text = msg;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+}
